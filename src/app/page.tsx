102f0ba7056d3d90fafd6de0f60e1125fff86482
muhsinsuny <muhsinsuny@gmail.com>
2025-05-15 17:52:16 +0700

    fix: comment out unused FAQSection import to clean up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import AboutMe from '@/home/partials/aboutme';
 import ExperienceTimeline from '@/home/partials/ExperienceTimeLine';
-import { FAQSection } from '@/home/partials/faq';
+// import { FAQSection } from '@/home/partials/faq';
 import { Hero } from '@/home/partials/hero';
 import Navbar from '@/home/partials/navbar';
 import PortfolioGrid from '@/home/partials/portfolio';
